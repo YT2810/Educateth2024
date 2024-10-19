1ad4cc7d4b9638019cae446d42d61b3942abf5ed
YT2810 <yolfry@gmail.com>
2024-10-19 00:53:50 -0400

    Tarea Sintaxis 2

--- HolaMundo.sol
+++ HolaMundo.sol
@@ -3,5 +3,5 @@
 pragma solidity 0.8.24;
 
 contract HolaMundo {
-    string public saludo="Testeando en Educateth";
+    string internal saludo="Testeando en Educateth";
 }
--- Hola_Mundo_Dinamico.sol
+++ Hola_Mundo_Dinamico.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
+import "./HolaMundo.sol";
 
-contract Hola_Mundo_Dinamico {
+contract Hola_Mundo_Dinamico is HolaMundo {
     string Saludo_d = "Hola Mundo";
     string public  Saludo_e = "SaludO inicial en el despliegue";
 
-    function leer_saludo () public view returns  (string memory) {
-        return Saludo_d;
+    function leer_saludo () internal view returns  (string memory) {
+        return saludo;
     }
 
     function guarda_saludo (string memory _nuevoSaludo) public {
         Saludo_d = _nuevoSaludo;
     }
 }
+
